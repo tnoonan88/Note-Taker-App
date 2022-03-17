117946e91daa4bad3aebc6786971702103f7afa5
tnoonan <tnoonan88@gmail.com>
2022-03-17 18:28:23 -0400

    built GET and POST routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,26 @@
-const express = require('express');
-const noteRouter = require('./db/db.json');
+const fb = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils.js');
+
+// GET route for retrieving notes
+fb.get('/notes', (req, res) => {
+    readFromFile('./db/db.json', 'utf8')
+    .then(data => {
+        console.log(data);
+        return res.json(JSON.parse(data));
+    })}
+);
+
+// POST route for submitting new note
+fb.post('/notes', (req, res) => {
+    console.log(req.body);
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        noteId: uuidv4(),
+    };
+    readAndAppend(newNote);
+    res.json(newNote);
+});
+
+module.exports = fb;
