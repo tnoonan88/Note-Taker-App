3760cd304de85b7b3f4871bdd0a1174f1dc37060
tnoonan <tnoonan88@gmail.com>
2022-03-17 18:30:29 -0400

    debugging

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ const app = express();
 // middleware for parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));       
+
 app.use('/api', api);
 
-app.use(express.static('public'));
+
+// GET route for notes.html
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // GET route for index.html
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-// GET route for notes.html
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, function () {
